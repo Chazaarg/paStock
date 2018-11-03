98370f033ebbb96afe81dbc52c63a395099c1b14
Patricio <chazarreta.patricio@gmail.com>
2018-11-03 16:38:56 -0300

    login endpoint

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,19 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/login", name="login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request)
     {
         $user = $this->getUser();
-
         return $this->json(array(
             'username' => $user->getUsername(),
             'roles' => $user->getRoles(),
