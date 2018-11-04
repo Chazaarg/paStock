b56003cb02465f95e42af17b05be6b09b2d9d16d
Patricio <chazarreta.patricio@gmail.com>
2018-11-04 14:56:38 -0300

    Debug

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class SecurityController extends AbstractController
 {
@@ -13,10 +15,58 @@ class SecurityController extends AbstractController
      */
     public function login(Request $request)
     {
+        
         $user = $this->getUser();
-        return $this->json(array(
-            'username' => $user->getUsername(),
-            'roles' => $user->getRoles(),
-        ));
+        $roles = $user->getRoles();
+        $name = $user->getUsername();
+        $email = $user->getEmail();
+        $id = $user->getID();
+
+        $role = [];
+        foreach ($roles as $rol){
+            $role += ['rol' => $rol];
+        }
+
+        // Call whatever methods you've added to your User class
+        // For example, if you added a getFirstName() method, you can use that.
+        return new JsonResponse(['id' => $id,'roles' => $role, 'username' => $name, 'email' => $email]);
+    }
+
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index()
+    {
+        // usually you'll want to make sure the user is authenticated first
+            
+
+        // returns your User object, or null if the user is not authenticated
+        // use inline documentation to tell your editor your exact User class
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        if ($user === null){
+            return new JsonResponse(['user' => 'anonymous']);
+        }
+
+        $roles = $user->getRoles();
+        $name = $user->getUsername();
+        $email = $user->getEmail();
+        $id = $user->getID();
+
+        $role = [];
+        foreach ($roles as $rol){
+            $role += ['rol' => $rol];
+        }
+
+        // Call whatever methods you've added to your User class
+        // For example, if you added a getFirstName() method, you can use that.
+        return new JsonResponse(['id' => $id,'roles' => $role, 'username' => $name, 'email' => $email]);
+    }
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(){
+        return new JsonResponse(JsonResponse::HTTP_OK);
     }
 }
--- src/Migrations/Version20181104175429.php
+++ src/Migrations/Version20181104175429.php
@@ -8,16 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180923161845 extends AbstractMigration
+final class Version20181104175429 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE producto DROP FOREIGN KEY FK_A7BB0615F9A084D5');
-        $this->addSql('DROP INDEX IDX_A7BB0615F9A084D5 ON producto');
-        $this->addSql('ALTER TABLE producto DROP variante_tipo_id');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
     }
 
     public function down(Schema $schema) : void
@@ -25,8 +23,7 @@ final class Version20180923161845 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE producto ADD variante_tipo_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE producto ADD CONSTRAINT FK_A7BB0615F9A084D5 FOREIGN KEY (variante_tipo_id) REFERENCES variante_tipo (id)');
-        $this->addSql('CREATE INDEX IDX_A7BB0615F9A084D5 ON producto (variante_tipo_id)');
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+        $this->addSql('ALTER TABLE user CHANGE roles roles VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
     }
 }
